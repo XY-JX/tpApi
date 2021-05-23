77ea1707f4636f6770a3c19880d214b898b573fa
xy <oncwnuK0WwGfyG9PFfQHBdZt3Y6o@git.weixin.qq.com>
2021-05-23 11:17:07 +0800

    优化代码

--- extend/Api.php
+++ extend/Api.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * code 可根据业务自行设置
  * 200成功，400失败，401数据验证错误，500 致命错误  （已内置状态码）
@@ -11,33 +12,36 @@ class Api
      * @param array $data 返回数据{}/[]
      * @param string $msg 返回描述
      */
-    private static function return(int $code=200,array $data=[],string $msg='success'){
+    private static function return(int $code = 200, array $data = [], string $msg = 'success')
+    {
         $result = [
             'code' => $code,
             'data' => $data,
-            'msg'  => $msg,
+            'msg' => $msg,
         ];
         throw new \think\exception\HttpResponseException(json($result));
     }
+
     /**
      * 成功 success
      * @param array $data
      * @param int $code
      * @param string $msg
      */
-    public static function success($data=[],$code=200,$msg='success')
+    public static function success($data = [], $code = 200, $msg = 'success')
     {
-        if(!is_array($data))$data = [$data];
-        return self::return($code,$data,$msg);
+
+        return self::return($code, is_array($data) ? $data : [$data], $msg);
     }
+
     /**
      * 失败 FAIL
      * @param string $msg
      * @param int $code
      * @param array $data
      */
-    public static function error($msg='FAIL',$code=400,$data=[])
+    public static function error($msg = 'FAIL', $code = 400, $data = [])
     {
-        return self::return($code,$data,$msg);
+        return self::return($code, $data, $msg);
     }
 }
