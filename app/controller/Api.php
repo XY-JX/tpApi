6033912e406b46bbe083b43ea1b5546ecb01ef53
zhangchunxu163 <zhangchunxu163@163.com>
2021-12-23 11:06:38 +0800

    export 导出需要安装额外扩展  composer require phpoffice/phpspreadsheet

--- app/controller/Api.php
+++ app/controller/Api.php
@@ -4,18 +4,27 @@ namespace app\controller;
 
 use app\BaseController;
 use  \xy_jx\Utils;
+
 class Api extends BaseController
 {
 
     public function index()
     {
-        var_dump( Utils\Sundry::orderNo());
+        var_dump(Utils\Sundry::orderNo());
     }
 
     public function cs()
     {
 
-        var_dump( Utils\Rmb::rmb_capital(15554.4));
+        var_dump(Utils\Rmb::rmb_capital(15554.4));
 
+    }
+
+    //export 导出需要安装额外扩展  composer require phpoffice/phpspreadsheet
+    public static function export($list)
+    {
+        \Excel::header('推广费用', ['plat_name' => '渠道', 'area_name' => '城市', 'account' => '账号', 'date' => '日期'], '共导出数据:' . count($list) . '条')
+            ->content($list)
+            ->save();
     }
 }
