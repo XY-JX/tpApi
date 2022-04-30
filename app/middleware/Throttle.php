dfe956dc0789e16333a0e6535a11c4d945917690
XY-JX <zhangschooi@qq.com>
2022-04-30 11:58:36 +0800

    限流

--- app/middleware/Throttle.php
+++ app/middleware/Throttle.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\middleware;
+
 use  \xy_jx\Utils\Sundry;
 
 class Throttle
@@ -11,13 +12,13 @@ class Throttle
      * @param \Closure $next
      * @param int $limit 指定时间内可请求的次数
      * @param string $time 时间范围 s|m|h|d
-     * @param string $prefix 分组和全局指定单独key
+     * @param string $key 分组和全局指定单独key
      * @return mixed
      * @throws \Exception
      */
-    public function handle($request, \Closure $next, int $limit = 3, string $time = 's', string $prefix = '')
+    public function handle($request, \Closure $next, int $limit = 3, string $time = 's', string $key = '')
     {
-        $key = $prefix ?? md5($request->ip() . $prefix . $request->baseUrl());
+        $key = $key ?? md5($request->ip() . $request->baseUrl());
         // 访问频率限制
         if (!Sundry::restrict([], $key, $limit, $time))
             \Api::fail('操作太频繁了', 429);
